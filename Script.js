f587579b75bb9fce8efafe8dff719cd66667f9c9
Roman <romanstepanovskyi@gmail.com>
2024-12-21 19:43:18 -0500

    initial commit

---
+++ Script.js
@@ -0,0 +1,67 @@
+/* let number = 5;
+const LeftBorderWidht = 1;
+
+
+number = 20;
+
+console.log(number);
+
+const obj = {
+    a: 50
+};
+
+console.log(
+    obj.a
+
+);
+ */
+
+
+/* 
+const arr = ['a', 'b', 'c'];
+const arrObj = {
+    0:'a',
+    1: 'b',
+    2: 'c'
+}
+
+
+arrObj.b = '12345';
+
+console.log(arrObj['b']);
+ */
+/* console.log(arr[1]);
+console.log(arrObj[1]);
+ */
+
+
+//alert ('Hello');
+
+//const result = confirm ('are you here?');
+
+//console.log(result);
+
+/* const answer = +prompt ("Are you 18?", "1");
+console.log(answer + 5 );
+ */
+ 
+
+/* const answers = [];
+
+/* answers [0] = prompt ('What is your name?', '');
+answers [1] = prompt ('how long do you live in Charlotte?', '');
+answers [2] = prompt ('Do you liked your job?', ''); */
+
+/* console.log(typeof(answers));
+ */
+/*  */ 
+
+
+
+const myName = 'Roman';
+
+alert(`Hi ,${myName}`);
+
+
+
+
